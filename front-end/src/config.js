095600c272d1d453f2b3c48015ed415473d6ff07
Standy Merizier <standy.merizier@gmail.com>
2021-09-07 00:23:09 -0400

    fixing config file

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,6 +1,6 @@
 export default {
   api: {
-      authUrl: 'https://accounts.spotify.com/authorize?client_id=<CLIENT_ID>&response_type=<TOKEN>&redirect_uri=http:%2F%2Flocalhost:3000',
+      authUrl: 'https://accounts.spotify.com/authorize?client_id=<CLIENT_ID>&response_type=token&redirect_uri=http:%2F%2Flocalhost:3000',
       baseUrl: 'https://api.spotify.com/v1',
       // authUrl: 'https://accounts.spotify.com/api/token',
       clientId: '',
